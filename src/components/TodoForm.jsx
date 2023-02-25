567fc7f9071ae16c971b5cc76721bca2b3137cd4
JeanLoic GARCIA <(mailto:garcia.jeanloic@gmail.com)>
2023-02-25 10:47:28 +0100

    Create todo list component without css and link todolist ans todoForm

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -9,10 +9,10 @@ const TodoForm = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*     props.onSubmit({
+    props.onSubmit({
       id: Math.floor(Math.random() * 10000),
       text: input,
-    }); */
+    });
     setInput("");
   };
   return (
